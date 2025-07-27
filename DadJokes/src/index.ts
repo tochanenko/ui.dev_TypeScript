c545b37deb8d9f5ccddc34f76ce9fd33120ea1be
Vladyslav Tochanenko <itochanenko@gmail.com>
2025-07-27 12:15:36 +0300

    Add About and 404 pages

--- DadJokes/src/index.ts
+++ DadJokes/src/index.ts
@@ -1,13 +1,24 @@
 import http, { IncomingMessage, ServerResponse } from 'http';
 import path from "path";
 import fs from "fs/promises";
+import url from "url";
 
 async function requitestListener(req: IncomingMessage, res: ServerResponse) {
-  const filePath = path.join(__dirname, "static/index.html");
+  const parsedUrl = url.parse(req.url||"");
 
-  const data = await fs.readFile(filePath, "utf-8");
+  let data = "";
+  let statusCode = 200;
+  try {
+    let pathName = parsedUrl.pathname;
+    if (pathName === "/") pathName = "/index";
+    const filePath = path.join(__dirname, `static${pathName}.html`);
+    data = await fs.readFile(filePath, "utf-8");
+  } catch {
+    data = await fs.readFile(path.join(__dirname, "static/404.html"), "utf-8");
+    statusCode = 404;
+  }
 
-  res.writeHead(200, {
+  res.writeHead(statusCode, {
     "content-type": "text/html",
     "content-length": data.length
   });
